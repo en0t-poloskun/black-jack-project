d60e3fea9daaafa76d02b6b3fdd89bb9d7faa07e
Мария <golovneva.maria@yandex.ru>
2021-05-06 14:24:06 +0300

    init user and dealer for game

---
+++ interface.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Interface
+  def name
+    print('Введите свое имя: ')
+    gets.chomp
+  end
+end
--- main.rb
+++ main.rb
@@ -3,6 +3,20 @@
 require_relative 'player'
 require_relative 'user'
 require_relative 'dealer'
+require_relative 'interface'
 
 class Main
+  def initialize
+    @interface = Interface.new
+  end
+
+  def blackjack
+    name = interface.name
+    user = User.new(name)
+    dealer = Dealer.new
+  end
+
+  private
+
+  attr_accessor :interface
 end
