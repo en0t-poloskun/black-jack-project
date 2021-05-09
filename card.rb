222793699f784fa8d7f01d3a47e7210c0a2e8015
Мария <golovneva.maria@yandex.ru>
2021-05-09 14:29:58 +0300

    constants moved to card class

--- card.rb
+++ card.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Card
+  CARD_VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  CARD_SUITS = ['+', '<3', '^', '<>'].freeze
+
   attr_reader :value, :suit
 
   def initialize(value, suit)
